28ec5d3d8fdbae85ea103900181c9beefc0c0d69
Thomas Gale <gale.thomasjames@gmail.com>
2020-02-25 21:20:02 +0000

    Rending the basic material UI example

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
 import { Provider } from 'react-redux'
 import { store } from './store'
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+
 import { App } from './views/components/App';
+import { theme } from './views/theme';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );

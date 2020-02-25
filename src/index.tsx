31dd783dcab46847cb2a4d99a00febe671e775d5
Thomas Gale <gale.thomasjames@gmail.com>
2020-02-25 21:09:17 +0000

    Moving component structure around

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import { Provider } from 'react-redux'
-import store from './store'
+import { store } from './store'
 
-import App from './App';
+import { App } from './views/components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(

02a674455e2f69e86c51d29efa57f174e080be50
Thomas Gale <gale.thomasjames@gmail.com>
2020-02-25 23:09:31 +0000

    WIP: Adding a linter

--- src/views/components/App.tsx
+++ src/views/components/App.tsx
@@ -1,36 +1,53 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, IconButton, Toolbar, Button } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
 import Link from '@material-ui/core/Link';
 
 import { ProTip } from './ProTip';
 import { Viewer } from './Viewer';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        bitstoatoms.io
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export function App() {
+  const classes = useStyles();
+
   return (
-    <Container maxWidth="sm">
+      <Grid container spacing={3}>
+            <AppBar position="static">
+            <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Bits to Atoms
         </Typography>
         <ProTip />
         <Viewer />
-        <Copyright />
       </Box>
-    </Container>
+    </Grid>
   );
 }

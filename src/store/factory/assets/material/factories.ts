b559fa6a80b1236c52a11d4627f2f30761b343ae
Thomas Gale <gale.thomasjames@gmail.com>
2020-03-09 18:05:49 +0000

    createSimplePolymerSpool() api updated

--- src/store/factory/assets/material/factories.ts
+++ src/store/factory/assets/material/factories.ts
@@ -1,6 +1,4 @@
-import { Identity } from '../../../common/identity/types';
 import { SimplePolymerSpool } from './types';
-import { Vector3, Quaternion, Cuboid } from '../../../common/primitive/types';
 
 import { createNewIdentity } from '../../../common/identity/factories';
 import {
@@ -8,33 +6,31 @@ import {
   createQuaternion,
   createCuboid
 } from '../../../common/primitive/factories';
-import { Material } from '../../../material/types';
 import { createSimplePolymerMaterial } from '../../../material/factories';
-import { NumberParameter } from '../../../common/parameter/types';
 import { createNumberParameter } from '../../../common/parameter/factories';
 import { FixedAssetType } from '../../../economic/types';
 
-export function createSimplePolymerSpool(
-  id: Identity = createNewIdentity({
+export function createSimplePolymerSpool({
+  id = createNewIdentity({
     displayName: 'default-simple-polymer-spool'
   }),
-  location: Vector3 = createVector3(),
-  orientation: Quaternion = createQuaternion(),
-  bounds: Cuboid = createCuboid(),
-  material: Material = createSimplePolymerMaterial(),
-  spoolLength: NumberParameter = createNumberParameter({
+  location = createVector3(),
+  orientation = createQuaternion(),
+  bounds = createCuboid(),
+  material = createSimplePolymerMaterial(),
+  spoolLength = createNumberParameter({
     identity: createNewIdentity({ displayName: 'Spool Length' }),
     units: 'm',
     value: 10
   }),
-  spoolDiameter: NumberParameter = createNumberParameter({
+  spoolDiameter = createNumberParameter({
     identity: createNewIdentity({ displayName: 'Spool Diameter' }),
     units: 'mm',
     value: 1
   }),
   depreciationRate = 0,
   dollars = 10
-): SimplePolymerSpool {
+}): SimplePolymerSpool {
   return {
     type: FixedAssetType.SimplePolymerSpool,
     id,

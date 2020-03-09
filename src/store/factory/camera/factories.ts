5c019827b5096dc4747fd4ef12559558913e94c1
Thomas Gale <gale.thomasjames@gmail.com>
2020-03-09 18:17:36 +0000

    CreateCameraTarget() api updated.

--- src/store/factory/camera/factories.ts
+++ src/store/factory/camera/factories.ts
@@ -1,13 +1,17 @@
 import * as THREE from 'three';
 import { CameraTarget } from './types';
 
-export function CreateDefaultCameraTarget(): CameraTarget {
+export function CreateCameraTarget({
+  position = new THREE.Vector3(0, 0, 5),
+  lookAt = new THREE.Vector3(0, 0, 0),
+  screenSpaceBounds = new THREE.Box2(
+    new THREE.Vector2(-4, -4),
+    new THREE.Vector2(4, 4)
+  )
+} = {}): CameraTarget {
   return {
-    position: new THREE.Vector3(0, 0, 5),
-    lookAt: new THREE.Vector3(0, 0, 0),
-    screenSpaceBounds: new THREE.Box2(
-      new THREE.Vector2(-4, -4),
-      new THREE.Vector2(4, 4)
-    )
+    position,
+    lookAt,
+    screenSpaceBounds
   };
 }

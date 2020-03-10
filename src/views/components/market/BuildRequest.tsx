ea3e69755a1f8b239aa099de9b8f281c45743978
Thomas Gale <gale.thomasjames@gmail.com>
2020-03-10 19:14:19 +0000

    Linted

--- src/views/components/market/BuildRequest.tsx
+++ src/views/components/market/BuildRequest.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { BuildRequest } from '../../../store/market/types';
-
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { Identity } from '../../../store/common/identity/types';
 import { Grid } from '@material-ui/core';
 
 interface Props {

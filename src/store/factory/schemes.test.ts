a7892b3d2b061f83264926c4e0abe2002e51e9f9
Thomas Gale <gale.thomasjames@gmail.com>
2020-03-20 23:28:24 +0000

    Started building the can normalise factory test properly.

--- src/store/factory/schemes.test.ts
+++ src/store/factory/schemes.test.ts
@@ -2,14 +2,14 @@ import { normalize } from 'normalizr';
 import { identitySchema, factorySchema } from './schemes';
 import { createExistingIdentity } from '../common/identity/factories';
 
-test('normalizr tests', () => {
+test('can normalize factory', () => {
   // Arrange
   const fixedUuid = '945430d8-0fc3-4fc5-9489-2875b7b70906';
   const testIdentity = createExistingIdentity({
     displayName: 'test-identity',
     id: fixedUuid
   });
-  const testFactory = { identity: testIdentity };
+  const testFactory = { id: testIdentity };
 
   // Act
   const normalizedFactory = normalize(testFactory, factorySchema);
@@ -17,13 +17,13 @@ test('normalizr tests', () => {
   // Assert
   expect(normalizedFactory).toEqual({
     entities: {
-      identity: {
+      identities: {
         '945430d8-0fc3-4fc5-9489-2875b7b70906': {
           displayName: 'test-identity',
           id: '945430d8-0fc3-4fc5-9489-2875b7b70906'
         }
       }
     },
-    result: { identity: '945430d8-0fc3-4fc5-9489-2875b7b70906' }
+    result: { id: '945430d8-0fc3-4fc5-9489-2875b7b70906' }
   });
 });
--- src/store/factory/schemes.ts
+++ src/store/factory/schemes.ts
@@ -1,8 +1,36 @@
 import { schema } from 'normalizr';
 
 // Test placing all schemes here. They will need moving to correct directories.
-export const identitySchema = new schema.Entity('identity');
+export const identitySchema = new schema.Entity('identities');
+
+export const activitySchema = new schema.Entity(
+  'activities',
+  {
+    id: identitySchema
+  },
+  {}
+);
+
+export const workflowSchema = new schema.Entity(
+  'workflows',
+  {
+    id: identitySchema,
+    activities: [activitySchema]
+  },
+  {}
+);
+
+export const buildRequestSchema = new schema.Entity(
+    'buildRequests',
+    {
+      id: identitySchema,
+      workflow: workflowSchema,
+      firstActivity: activitySchema
+    },
+    {}
+  );
 
 export const factorySchema = {
-  identity: identitySchema
+  id: identitySchema,
+  buildRequests: [buildRequestSchema]
 };

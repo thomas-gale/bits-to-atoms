102bdfb906e35b0f888ffde71ab5ea9e3dbef862
Thomas Gale <gale.thomasjames@gmail.com>
2020-03-23 21:26:51 +0000

    Adding experiemental testing library for tsx element.

---
+++ src/views/components/market/BuildRequestSummary.test.tsx
@@ -0,0 +1,30 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { render, waitForElement } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { rootReducer } from '../../../store';
+import { createBuildRequest } from '../../../store/buildrequest/factories';
+import BuildRequestSummary from './BuildRequestSummary';
+
+test('loads and displays build summary that can bid', async () => {
+  // Arrange
+  const isAllowedToBid = true;
+  const buildRequest = createBuildRequest();
+  const store = configureStore({
+    reducer: rootReducer
+  });
+
+  // Act
+  const { getByText } = render(
+    <Provider store={store}>
+      <BuildRequestSummary
+        isAllowedToBid={isAllowedToBid}
+        buildRequest={buildRequest}
+      />
+    </Provider>
+  );
+
+  // Assert
+  expect(getByText(buildRequest.id)).toHaveTextContent(buildRequest.id);
+  expect(getByText(buildRequest.displayName)).toHaveTextContent(buildRequest.displayName);
+});

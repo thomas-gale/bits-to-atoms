af94ca68b10939a1337b2860a895a60c3b4b491c
Thomas Gale <gale.thomasjames@gmail.com>
2020-03-28 09:55:59 +0000

    fix(Added Cypress Market e2e Tests): Using the Cypress API to find build request bid button.

---
+++ cypress/integration/market/market.spec.ts
@@ -0,0 +1,18 @@
+context('Market', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('get the first build request summary', () => {
+    cy.wait(100);
+    cy.contains('Build Request');  
+  });
+
+  it('bid on first build request summary', () => {
+    // Act
+    cy.wait(100);
+    cy.contains('Build Request').parent().find('button').click();  
+    cy.wait(100);
+  });
+
+});

534fd47764f217ec04b9fd1b8e3e8f328609e45f
Thomas Gale <gale.thomasjames@gmail.com>
2020-09-11 23:37:23 +0100

    WIP: Downloading the FFFModel data from IFPS.
    However, stuggling at the last hurdle with the react-three-fibre useLoader hook and raw data as gltf.

---
+++ src/store/ipfs/use-ipfs.ts
@@ -0,0 +1,27 @@
+// Derived from example: https://github.com/ipfs/js-ipfs/blob/master/examples/browser-create-react-app/src/hooks/use-ipfs.js
+import { useState, useEffect } from 'react';
+import dotProp from 'dot-prop';
+
+/*
+ * Pass the command you'd like to call on an ipfs instance.
+ *
+ * Uses setState to capture the response, so your component
+ * will re-render when the result turns up.
+ *
+ */
+export default function useIpfs(ipfs: any, cmd: any, opts: any) {
+  const [res, setRes] = useState(null);
+  useEffect(() => {
+    callIpfs(ipfs, cmd, opts, setRes);
+  }, [ipfs, cmd, opts]);
+  return res;
+}
+
+async function callIpfs(ipfs: any, cmd: any, opts: any, setRes: any) {
+  if (!ipfs) return null;
+  console.log(`Call ipfs.${cmd}`);
+  const ipfsCmd: any = dotProp.get(ipfs, cmd);
+  const res = await ipfsCmd(opts);
+  console.log(`Result ipfs.${cmd}`, res);
+  setRes(res);
+}
--- src/views/components/factory/services/FFFPrinter.tsx
+++ src/views/components/factory/services/FFFPrinter.tsx
@@ -1,7 +1,8 @@
-import React, { useMemo, useState, Suspense } from 'react';
-import { Euler, Quaternion as ThreeQuaternion } from 'three';
+import React, { useMemo, useState, Suspense, useEffect, useRef } from 'react';
+import { Euler, Quaternion as ThreeQuaternion, Mesh } from 'three';
 import FFFPrinterModel from './models/FFFPrinter.gltf';
 import { FFFPrinter } from '../../../../store/factory/services/fffprinter/types';
+import useIpfsFactory from '../../../../store/ipfs/use-ipfs-factory';
 
 type OwnProp = {
   fffPrinter: FFFPrinter;
@@ -14,7 +15,8 @@ type OwnDispatch = {
 type Props = OwnProp & OwnDispatch;
 
 export function FFFPrinterElement(props: Props): JSX.Element {
-  const { id, location, orientation } = props.fffPrinter;
+  const mesh = useRef<Mesh>();
+  const { id, location, orientation, bounds } = props.fffPrinter;
 
   // React hooks for converting the Quaterion into Euler angles.
   const [eulerRotation, setEulerRotation] = useState<Euler>(new Euler(0, 0, 0));
@@ -31,16 +33,64 @@ export function FFFPrinterElement(props: Props): JSX.Element {
     setEulerRotation(newEuler);
   }, [orientation]);
 
-  return (
-    <Suspense fallback={null}>
-      <FFFPrinterModel
+  // Experimental file loading from ipfs
+  const { ipfs } = useIpfsFactory();
+
+  //const ipfsId = useIpfs(ipfs, 'id', null);
+  const [model, setModel] = useState<any | undefined>(undefined);
+
+  const modelCID = 'QmbdSeJfWsUm5ghb9pp98qwnbTbzKN8BvtarJhRTmTxhCw';
+
+  useEffect(() => {
+    const downloadModel = async () => {
+      const chunks = [];
+      for await (const chunk of ipfs.cat(modelCID)) {
+        chunks.push(chunk);
+        console.log('Got model chunk...');
+      }
+      console.log('All chunks downloaded! length: ' + chunks.length);
+      setModel(chunks);
+    };
+    if (ipfs) downloadModel();
+  }, [ipfs]);
+
+  if (model) {
+    return (
+      <Suspense fallback={null}>
+        <FFFPrinterModel
+          model={model}
+          position={[location.x, location.y, location.z]}
+          rotation={eulerRotation}
+          onClick={(e: any) => {
+            e.stopPropagation();
+            props.onSelected(id);
+          }}
+        />
+      </Suspense>
+    );
+  } else {
+    return (
+      <mesh
+        castShadow
+        receiveShadow
         position={[location.x, location.y, location.z]}
         rotation={eulerRotation}
+        ref={mesh}
         onClick={(e) => {
           e.stopPropagation();
           props.onSelected(id);
         }}
-      />
-    </Suspense>
-  );
+      >
+        <boxBufferGeometry
+          attach="geometry"
+          args={[
+            bounds.max.x - bounds.min.x,
+            bounds.max.y - bounds.min.y,
+            bounds.max.z - bounds.min.z,
+          ]}
+        />
+        <meshStandardMaterial attach="material" color={'grey'} />
+      </mesh>
+    );
+  }
 }

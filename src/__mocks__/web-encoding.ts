b101d6eedf25ec284dc8d6cff5c7a8c2587c1bee
Thomas Gale <gale.thomasjames@gmail.com>
2020-10-24 14:29:41 +0100

    fix(Global Encoder Mocks): Global Encoder Mocks
    Moving global encoder type mocks to setupTests.

--- src/__mocks__/web-encoding.ts
+++ src/__mocks__/web-encoding.ts
@@ -1,9 +1,5 @@
 import { TextEncoder, TextDecoder } from 'util';
 
-// Mock TextEncoding/Decoder in global.
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-
 // https://jestjs.io/docs/en/manual-mocks#mocking-node-modules
 // Mock TextEncoding/Decoder in web-encoding module.
 const webenc: any = jest.createMockFromModule('web-encoding');
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// Thanks to https://gist.github.com/indiesquidge/dfb136f0ebf95a2d7ee3b7f9687f4df5
+// Mock TextEncoding/Decoder in global.
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
